cf6cd226bc70931441b0b3a7f51c98ce2121f81a
a <pswxsj@naver.com>
2021-12-13 08:48:38 +0900

    서강대 추가

--- Score_by_College.py
+++ Score_by_College.py
@@ -10,19 +10,21 @@ eng_yon = [0, 0.5, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0]
 eng_ko = [0, 3, 6, 9, 12, 15, 18, 21, 24]
 eng_han = [100, 98, 94, 88, 80, 70, 58, 44, 28]
 eng_jung = [100, 98, 94, 88, 80, 70, 58, 44, 28]
+eng_su = [100, 99, 98, 97, 96, 95, 94, 93, 92]
 
 han_seo = [0, 0, 0, 0.4, 0.8, 1.2, 1.6, 2.0, 2.4]
 han_yon = [10, 10, 10, 10, 9.8, 9.6, 9.4, 9.2, 9.0]
 han_ko = [10, 10, 10, 10, 9.8, 9.6, 9.4, 9.2, 8.0]
 han_han = [0, 0, 0, 0, 0.1, 0.2, 0.3, 0.4, 0.5]
 han_jung = [10, 10, 10, 10, 10, 9.6, 9.2, 8.8, 8.4]
+han_su = [10, 10, 10, 10, 9.5, 9.0, 8.5, 8.0, 7.5]
 
 byun = pd.read_excel('./2022_변환표점.xls')
 korean, math, inquiry_std, inquiry_ba1, inquiry_ba2, english, history =\
     map(int, input("1. 국어 표점 2. 수학 표점 3. 탐구 표점 합 4. 탐구1 백분위 5. 탐구2 백분위 6. 영어 등급 7. 한국사 등급 순으로 입력해주세요 >").split())
 
 def byun_finder(uni, inquiry1, inquiry2):
-    if len(byun[byun['대학명']==uni]['계열코드'].unique()) == 2:
+    if (len(byun[byun['대학명']==uni]['계열코드'].unique()) == 2)| (len(byun[byun['대학명']==uni]['계열코드'].unique()) == 1):
         return byun[ (byun['대학명']==uni) & (byun['계열코드'] == '자연') & (byun['백분위'] == inquiry1)]['변환점수'].values[0]\
                    + byun[ (byun['대학명']==uni) & (byun['계열코드'] == '자연') & (byun['백분위'] == inquiry2)]['변환점수'].values[0]
 
@@ -50,7 +52,15 @@ def cal_score(korean, math, inquiry_std, inquiry_ba1, inquiry_ba2, english, hist
     #중앙대
     jung_byun = byun_finder('중앙대', inquiry_ba1, inquiry_ba2)
     score_by_college['중앙대'] = korean*0.4*5 + math*0.4*5 + jung_byun*0.2*5 +eng_jung[english-1]+han_jung[history-1]
+
+    #서강대
+    su_byun = byun_finder('서강대', inquiry_ba1, inquiry_ba2)
+    print(su_byun)
+    print(korean*1.1 + math*1.3 + su_byun*0.6 + eng_su[english-1] + han_su[history-1])
+    score_by_college['서강대'] = korean*1.1 + math*1.3 + su_byun*0.6 + eng_su[english-1] + han_su[history-1]
     return score_by_college
+
+
     # score_by_college['한양대'] = #
 
 score_by_college = pd.Series(dtype=float)
